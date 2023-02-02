a5d11f7a674126892f31dfcaef62f21a4fbbdfc0
Anton Protopopov <antpro@basealt.ru>
2023-02-02 21:42:39 +0400

    fix

--- 3_3_string_append.cpp
+++ 3_3_string_append.cpp
@@ -22,18 +22,18 @@ struct CustomString
     /* Реализуйте этот метод. */
 //    void append1(CustomString &other)
 //    {
-//        char* str2 = new char[size + other.size()];
+//        char* str2 = new char[size + other.size];
 //        for(size_t i = 0; i < size; ++i)
 //        {
 //            str2[i] = str[i];
 //        }
 
-//        for(size_t j = size+1; j < size + other.size(); ++j)
+//        for(size_t j = size+1; j < size + other.size; ++j)
 //        {
 //            str2[j] = other[j];
 //        }
 
-//        str[size + other.size() + 1] = '\0';
+//        str[size + other.size + 1] = '\0';
 ////        std::cout << "Result string: " << str << std::endl;
 ////        std::cout << str << std::endl;
 //    }
@@ -51,15 +51,21 @@ struct CustomString
 
     void app3(CustomString &other)
     {
-//            const char* str = "Take the test.";
-        //  src[0] = 'M'; // can't modify string literal
-        size_t new_size = size + other.size;
-        auto dst = std::make_unique<char[]>(std::strlen(str)+1); // +1 for the null terminator
-//        std::strcpy(dst.get(), str);
-        std::strcpy(dst.get() + size, other.str);
-//        dst[0] = 'M';
-        dst[new_size] = '\0';
-        std::cout << str << '\n' << dst.get() << '\n';
+//        char* dst = new char[size];
+        char* dst = new char[size + other.size];
+        strcpy(dst, str);
+        strcpy(&dst[size + 1], &other);
+//        dst* = char[size + 1];
+        strcat(dst, &other);
+//        dst[size + other.size] = ;
+//        for(size_t i = size + 1; i <= size + other.size; ++i)
+//        {
+//            dst[i] = other[i];
+//        }
+//        std::memcpy(dst, &other, size);
+//        std::strcat(dst, other);
+        dst[size + other.size + 1] = '\0';
+        std::cout << str << '\n' << dst << '\n';
     }
 
 };
@@ -89,9 +95,9 @@ CustomString::CustomString(size_t n, char c)
 int main()
 {
     CustomString s1("Hello,");
-    CustomString s2(" world!");
+    CustomString s2(" world123");
 
-    s1.append2(s2);
+//    s1.append2(s2);
     s1.app3(s2);
 
 }
